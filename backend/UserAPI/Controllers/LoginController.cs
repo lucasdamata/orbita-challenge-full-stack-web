8d45a594b779bcc10eaba9300924084e895f1287
Lucas da Mata Silva <hefesttecnologia@gmail.com>
2022-03-28 17:06:34 -0300

    chore: add project users

---
+++ backend/UserAPI/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using UserAPI.Data.Requests;
+using UserAPI.Services;
+
+namespace UserAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private LoginService _loginService;
+
+        public LoginController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public IActionResult Singnin(LoginRequest request)
+        {
+            Result result = _loginService.Singnin(request);
+            if (result.IsFailed) return Unauthorized();
+            return Ok();
+        }
+    }
+}
---
+++ backend/UserAPI/Services/RegisterService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+using UserAPI.Controllers;
+using UserAPI.Models;
+
+namespace UserAPI.Services
+{
+    public class RegisterService
+    {
+        private IMapper _mapper;
+        private UserManager<IdentityUser<int>> _userManager;
+
+        public RegisterService(IMapper mapper, UserManager<IdentityUser<int>> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        public Result Create(CreateUserDTO userDTO)
+        {
+            User user = _mapper.Map<User>(userDTO);
+            IdentityUser<int> userIdentity = _mapper.Map<IdentityUser<int>>(user);
+            Task<IdentityResult> resultIdentity = _userManager.CreateAsync(userIdentity, userDTO.Password);
+            if (resultIdentity.Result.Succeeded) return Result.Ok();
+            return Result.Fail("Error in register user");
+        }
+    }
+}
